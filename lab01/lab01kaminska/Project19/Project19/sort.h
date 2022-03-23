b30ba6f93c5a6dacf9c0d3190fffa39f08483534
Liubomyr Halamaha <galamagal86@gmail.com>
2022-03-23 10:29:15 +0200

    add lab 01 kaminska

---
+++ lab01/lab01kaminska/Project19/Project19/sort.h
@@ -0,0 +1,10 @@
+#pragma once
+
+void printArray(int* arr, int n);
+void swap(int* a, int* b);
+void reverseArray(int* arr, int n);
+void bubbleSort(int* arr, int n);
+void quickSort(int* arr, int left, int right);
+int getMax(int* arr, int n);
+void countSort(int* arr, int n);
+void shellSort(int* arr, int n);
