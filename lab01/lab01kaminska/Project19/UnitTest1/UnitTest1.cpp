a00b0d7b4a28d203eaa9d0e6078d2ca23b22a6bd
zxc <yuliia.kaminska@gmail.com>
2022-03-25 14:21:28 +0200

    Update UnitTest1.cpp

--- lab01/lab01kaminska/Project19/UnitTest1/UnitTest1.cpp
+++ lab01/lab01kaminska/Project19/UnitTest1/UnitTest1.cpp
@@ -10,108 +10,112 @@ namespace UnitTest1
 	{
 	public:
 		
-		TEST_METHOD(TestBubbleSort){
+		TEST_METHOD(TestGetMax){
 			const int size = 6;
 			int array[size] = { 6,2,7,1,9,4 };
-			Assert::AreEqual(array[0], 6);
-			Assert::AreEqual(array[2], 7);
 
 			Assert::AreEqual(getMax(array, size), 9);
+		}
 
-			reverseArray(array, size);
-			Assert::AreEqual(array[0], 4);
-			Assert::AreEqual(array[2], 1);
-
-			bubbleSort(array, size);
-			Assert::AreEqual(array[0], 1);
-			Assert::AreEqual(array[1], 2);
-			Assert::AreEqual(array[2], 4);
-
-			reverseArray(array, size);
-			Assert::AreEqual(array[0], 9);
-			Assert::AreEqual(array[1], 7);
+		TEST_METHOD(TestSwap) {
+			const int size = 6;
+			int array[size] = { 6,2,7,1,9,4 };
 
 			swap(&array[0], &array[1]);
-			Assert::AreEqual(array[0], 7);
-			Assert::AreEqual(array[1], 9);
+			Assert::AreEqual(array[0], 2);
+			Assert::AreEqual(array[1], 6);
+		}
+
+		TEST_METHOD(TestBubbleSort) {
+			const int size = 6;
+			int array[size] = { 6,2,7,1,9,4 };
+			int reversedArray[size] = { 4,9,1,7,2,6 };
+			int sortedArray[size] = { 1,2,4,6,7,9 };
+			int reversedSortedArray[size] = { 9,7,6,4,2,1 };
+
+			reverseArray(array, size);
+			for (int i = 0; i < size; ++i) {
+				Assert::AreEqual(array[i], reversedArray[i]);
+			}
+
+			bubbleSort(array, size);
+			for (int i = 0; i < size; ++i) {
+				Assert::AreEqual(array[i], sortedArray[i]);
+			}
+
+			reverseArray(array, size);
+			for (int i = 0; i < size; ++i) {
+				Assert::AreEqual(array[i], reversedSortedArray[i]);
+			}
 		}
 
 		TEST_METHOD(TestQuickSort) {
 			const int size = 6;
 			int array[size] = { 6,2,7,1,9,4 };
-			Assert::AreEqual(array[0], 6);
-			Assert::AreEqual(array[2], 7);
-
-			Assert::AreEqual(getMax(array, size), 9);
+			int reversedArray[size] = { 4,9,1,7,2,6 };
+			int sortedArray[size] = { 1,2,4,6,7,9 };
+			int reversedSortedArray[size] = { 9,7,6,4,2,1 };
 
 			reverseArray(array, size);
-			Assert::AreEqual(array[0], 4);
-			Assert::AreEqual(array[2], 1);
+			for (int i = 0; i < size; ++i) {
+				Assert::AreEqual(array[i], reversedArray[i]);
+			}
 
 			quickSort(array, 0, size - 1);
-			Assert::AreEqual(array[0], 1);
-			Assert::AreEqual(array[1], 2);
-			Assert::AreEqual(array[2], 4);
+			for (int i = 0; i < size; ++i) {
+				Assert::AreEqual(array[i], sortedArray[i]);
+			}
 
 			reverseArray(array, size);
-			Assert::AreEqual(array[0], 9);
-			Assert::AreEqual(array[1], 7);
-
-			swap(&array[0], &array[1]);
-			Assert::AreEqual(array[0], 7);
-			Assert::AreEqual(array[1], 9);
+			for (int i = 0; i < size; ++i) {
+				Assert::AreEqual(array[i], reversedSortedArray[i]);
+			}
 		}
 
 		TEST_METHOD(TestCountSort) {
 			const int size = 6;
 			int array[size] = { 6,2,7,1,9,4 };
-			Assert::AreEqual(array[0], 6);
-			Assert::AreEqual(array[2], 7);
-
-			Assert::AreEqual(getMax(array, size), 9);
+			int reversedArray[size] = { 4,9,1,7,2,6 };
+			int sortedArray[size] = { 1,2,4,6,7,9 };
+			int reversedSortedArray[size] = { 9,7,6,4,2,1 };
 
 			reverseArray(array, size);
-			Assert::AreEqual(array[0], 4);
-			Assert::AreEqual(array[2], 1);
+			for (int i = 0; i < size; ++i) {
+				Assert::AreEqual(array[i], reversedArray[i]);
+			}
 
 			countSort(array, size);
-			Assert::AreEqual(array[0], 1);
-			Assert::AreEqual(array[1], 2);
-			Assert::AreEqual(array[2], 4);
+			for (int i = 0; i < size; ++i) {
+				Assert::AreEqual(array[i], sortedArray[i]);
+			}
 
 			reverseArray(array, size);
-			Assert::AreEqual(array[0], 9);
-			Assert::AreEqual(array[1], 7);
-
-			swap(&array[0], &array[1]);
-			Assert::AreEqual(array[0], 7);
-			Assert::AreEqual(array[1], 9);
+			for (int i = 0; i < size; ++i) {
+				Assert::AreEqual(array[i], reversedSortedArray[i]);
+			}
 		}
 
 		TEST_METHOD(TestShellSort) {
 			const int size = 6;
 			int array[size] = { 6,2,7,1,9,4 };
-			Assert::AreEqual(array[0], 6);
-			Assert::AreEqual(array[2], 7);
-
-			Assert::AreEqual(getMax(array, size), 9);
+			int reversedArray[size] = { 4,9,1,7,2,6 };
+			int sortedArray[size] = { 1,2,4,6,7,9 };
+			int reversedSortedArray[size] = { 9,7,6,4,2,1 };
 
 			reverseArray(array, size);
-			Assert::AreEqual(array[0], 4);
-			Assert::AreEqual(array[2], 1);
+			for (int i = 0; i < size; ++i) {
+				Assert::AreEqual(array[i], reversedArray[i]);
+			}
 
 			shellSort(array, size);
-			Assert::AreEqual(array[0], 1);
-			Assert::AreEqual(array[1], 2);
-			Assert::AreEqual(array[2], 4);
+			for (int i = 0; i < size; ++i) {
+				Assert::AreEqual(array[i], sortedArray[i]);
+			}
 
 			reverseArray(array, size);
-			Assert::AreEqual(array[0], 9);
-			Assert::AreEqual(array[1], 7);
-
-			swap(&array[0], &array[1]);
-			Assert::AreEqual(array[0], 7);
-			Assert::AreEqual(array[1], 9);
+			for (int i = 0; i < size; ++i) {
+				Assert::AreEqual(array[i], reversedSortedArray[i]);
+			}
 		}
 	};
 }
