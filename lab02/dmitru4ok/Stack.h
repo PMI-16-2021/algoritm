10e209e6b8bdab6d4ea0ce065e6c9bf6c02d1561
dmitru4ok <mobilokh4566@gmail.com>
2022-03-26 19:07:31 +0200

    Added Struct for List Node
    added basic stack functions

--- lab02/dmitru4ok/Stack.h
+++ lab02/dmitru4ok/Stack.h
@@ -1,5 +1,29 @@
 #pragma once
+#include <iostream>
 class Stack
 {
+public:
+	struct StackNode
+	{
+		int item;
+		StackNode* next;
+		StackNode(int _item, StackNode* _next = nullptr) : item(_item), next(_next) {}
+
+	};
+private:
+	StackNode* Top;
+	void delFirst();
+public:
+	Stack(): Top(nullptr){}
+	Stack(int _first_item): Top(new StackNode(_first_item, nullptr)){}
+	~Stack(); // uses empty
+
+	void push(int); //add new node with paraeter value
+	void clear(); //clear all nodes
+	int pop(); // return item and remove first node
+	bool empty(); // check if is empty
+	size_t size();
+	void printStack();
+	int top();
 };
 
--- lab02/dmitru4ok/main.cpp
+++ lab02/dmitru4ok/main.cpp
@@ -6,10 +6,20 @@ using std::cout;
 
 int main()
 {
+	Stack myStack;
+	for (int i = 0; i < 6; ++i)
+	{
+		myStack.push(i);
+	}
+	cout << "Size: " << myStack.size() << "Top element:" << myStack.top() << '\n';
 
-
-
-
+	while (!myStack.empty())
+	{
+		cout << myStack.pop() << '\n';
+	}
+	cout << "Size: " << myStack.size() << '\n';
+	
+	
 
 
 
