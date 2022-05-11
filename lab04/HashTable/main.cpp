b0e85b6a276f28ad044552a6e3c8c160935c1c48
Voridan <bogdanvorobienko@gmail.com>
2022-05-11 11:17:47 +0300

    changes

--- lab04/HashTable/main.cpp
+++ lab04/HashTable/main.cpp
@@ -4,6 +4,7 @@
 int main() {
     HashTable<int> table;
     TableElem<int> elem1("lolo", 2);
+    TableElem<int> elem0("olol", 2);
     TableElem<int> elem2("l", 45);
     TableElem<int> elem3("paul", 35);
     TableElem<int> elem4("rebeka", 5);
@@ -14,6 +15,7 @@ int main() {
     TableElem<int> elem9("horror", 9);
     TableElem<int> elem10("sdw", 8);
 
+    table.Add(elem0);
     table.Add(elem1);
     table.Add(elem2);
     table.Add(elem3);
@@ -22,7 +24,7 @@ int main() {
     table.Add(elem6);
     table.Add(elem7);
     table.Add(elem8);
-    table.Add(elem9);
+//    table.Add(elem9);
     table.Add(elem10);
 
     std::cout<<table.Get("lolo")<<'\n';
@@ -33,7 +35,8 @@ int main() {
     std::cout<<table.Get("leleka")<<'\n';
     std::cout<<table.Get("richka")<<'\n';
     std::cout<<table.Get("qweer")<<'\n';
-    std::cout<<table.Get("horror")<<'\n';
+//    std::cout<<table.Get("horror")<<'\n';
+    std::cout<<table.Get("olol")<<'\n';
     std::cout<<table.Get("sdw")<<'\n';
     std::cout<<'\n';
     table.Values();
