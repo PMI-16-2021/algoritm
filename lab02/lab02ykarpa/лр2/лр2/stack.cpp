2d292b097122a457d2cba7ab96d2ee45e3aeab31
ykarpa <99607003+ykarpa@users.noreply.github.com>
2022-05-11 18:08:58 +0300

    Update stack.cpp

--- lab02/lab02ykarpa/лр2/лр2/stack.cpp
+++ lab02/lab02ykarpa/лр2/лр2/stack.cpp
@@ -1,79 +1,28 @@
-#include "Stack.h"
+#pragma once
+#include <iostream>
 
-//Node::Node(int NodeValue, Node* Nodeptr) : value(NodeValue), ptr(Nodeptr) {}
-NodeStack::NodeStack(const int nodeValue, NodeStack* nodePtr)
-    : value_(nodeValue), ptr_(nodePtr) {
+struct NodeStack {
+    NodeStack* ptr_;
+    int value_;
+    NodeStack(const int nodeValue, NodeStack* nodeptr);
+};
 
-}
+class Stack {
+private:
+    NodeStack* top_;
 
-Stack::Stack() {
-    top_ = nullptr;
-}
+public:
+    Stack* nextStack = nullptr;
 
-Stack::Stack(const int value) {
-    top_ = new NodeStack(value, nullptr);
-}
+    Stack();
+    Stack(const int value);
 
-bool Stack::isEmpty() const {
-    return top_ == nullptr;
-}
-
-int Stack::getSize() const {
-    int counter = 0;
-    NodeStack* tempPtr = top_;
-
-    if (top_ == nullptr) {
-        return 0;
-    }
-    else {
-        while (tempPtr != nullptr) {
-            ++counter;
-            tempPtr = tempPtr->ptr_;
-        }
-    }
-    delete tempPtr;
-    return counter;
-}
-
-int Stack::top() const {
-    return top_->value_;
-}
-
-void Stack::push(const int value) {
-    NodeStack* tempPtr = new NodeStack(value, top_);
-    top_ = tempPtr;
-}
-
-void Stack::emplace(const int value) {
-    NodeStack* tempPtr = new NodeStack(value, top_);
-    top_ = tempPtr;
-}
-
-void Stack::pop() {
-        NodeStack* tempPtr;
-        int temp;
-        temp = top_->value_;
-        tempPtr = top_;
-        top_ = top_->ptr_;
-        delete tempPtr;
-}
-
-void Stack::swap(Stack& stack) {
-    NodeStack* buffer = top_;
-    this->top_ = stack.top_;
-    stack.top_ = buffer;
-}
-
-void Stack::print() const {
-    if (top_ == nullptr) {
-        std::cout << "\nStack is empty!\n";
-        return;
-    }
-    std::cout << "Stack: ";
-    NodeStack* tempPtr = top_;
-    while (tempPtr != nullptr) {
-        std::cout << tempPtr->value_ << ' ';
-        tempPtr = tempPtr->ptr_;
-    }
-    std::cout << "\n";
-}
+    bool isEmpty() const;
+    int getSize() const;
+    int top() const;
+    void push(const int value);
+    void emplace(const int value);
+    void pop();
+    void swap(Stack& stack);
+    void print() const;
+};
