b0701b3fbb834d95f5e8f8be8864f29843300cf8
dmitru4ok <mobilokh4566@gmail.com>
2022-05-22 23:55:25 +0300

    Added inplementation of BSAVL tree

--- lab10/AVL/main.cpp
+++ lab10/AVL/main.cpp
@@ -1,19 +1,23 @@
 #include "AVLTree.h"
-
+using std::cout;
 
 int main()
 {
-
-
-
-
-
-
-
-
-
-
-
+	Tree<int> A;
+	
+	cout << "\nheight: " << A.height() << '\n';
+	A.insert(1);
+	A.insert(2);
+	cout << "\nheight: " << A.height() << '\n';
+	A.print(); 
+	A.insert(3);
+	A.insert(4);
+	A.insert(6);
+	A.insert(2);
+	A.insert(8);
+	cout << "\nheight: " << A.height() << '\n';
+	A.print();
+	
 
 
 
