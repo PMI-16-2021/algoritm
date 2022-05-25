69204aa8a36ead6eb72c91e4e52379f2220456e4
Pavlo Kikis <99750479+n3xtor@users.noreply.github.com>
2022-05-25 23:07:17 +0300

    Update sorts.h

--- lab01/lab01n3xtor/SortingAlgoritms/sorts.h
+++ lab01/lab01n3xtor/SortingAlgoritms/sorts.h
@@ -3,5 +3,5 @@ void printArray(int arr[], size_t arrSize);
 
 void bubbleSort(int arr[], size_t arrSize);
 void quickSort(int* arr, size_t const arrSize);
-void countingSort(int arr[], size_t arrSize);countingSort
+void countingSort(int arr[], size_t arrSize);
 void shellSort(int arr[], size_t arrSize);
