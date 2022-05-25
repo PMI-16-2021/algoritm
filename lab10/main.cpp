0ccda652dcbd82dfc708f102ed2d3cb460d3bc0f
Voridan <bogdanvorobienko@gmail.com>
2022-05-25 16:35:23 +0300

    finished lab 10

---
+++ lab10/main.cpp
@@ -0,0 +1,10 @@
+#include "avl_tree.h"
+#include "tests.h"
+int main() {
+    TestInsert();
+    TestIsIn();
+    TestRemove();
+    TestGet();
+    TestHeight();
+    return 0;
+}
---
+++ lab10/tests.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "avl_tree.h"
+
+void TestInsert();
+void TestIsIn();
+void TestRemove();
+void TestGet();
+void TestHeight();
