457c199b198661a8431863e1486bace03faf6d56
Pavlo Kikis <99750479+n3xtor@users.noreply.github.com>
2022-05-29 00:36:34 +0300

    lab03n3xtor

---
+++ lab03/lab03n3xtor/main.cpp
@@ -0,0 +1,16 @@
+#include "list.h"
+#include "priorityQueue.h"
+#include "tests.h"
+
+int main()
+{
+    sizeTest();
+    topTest();
+    emptyTest();
+    clearTest();
+    enqueueTest();
+    dequeueTest();
+
+    system("pause>>void");
+    return 0;
+}
