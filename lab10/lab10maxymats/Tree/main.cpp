505cf151fd18d9a7a7b56de3a07563eb5338b1d7
maxymats <maksym.mats@gmail.com>
2022-05-31 02:39:48 +0300

    add task 10

---
+++ lab10/lab10maxymats/Tree/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Tree.h"
+int main() {
+    Node* root = NULL;
+    root = insertNode(root, 33);
+    root = insertNode(root, 13);
+    root = insertNode(root, 53);
+    root = insertNode(root, 9);
+    root = insertNode(root, 21);
+    root = insertNode(root, 61);
+    root = insertNode(root, 8);
+    root = insertNode(root, 11);
+    printTree(root, "", true);
+    root = deleteNode(root, 13);
+    cout << "After deleting " << endl;
+    printTree(root, "", true);
+
+    
+
+    system("pause");
+    return 0;
+}
---
+++ lab10/lab10maxymats/TreeTest/TreeTest.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tree/Tree.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TreeTest
+{
+	TEST_CLASS(TreeTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
