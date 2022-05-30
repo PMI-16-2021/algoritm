8d4cc6365b01b39bea4839fee09cd28b735c9359
Voridan <bogdanvorobienko@gmail.com>
2022-05-31 00:36:16 +0300

    deleted not required files

--- lab09/Graph/list.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Богдан Воробієнко on 30.05.2022.
-//
-
-#ifndef GRAPH_LIST_H
-#define GRAPH_LIST_H
-
-#endif //GRAPH_LIST_H
--- lab09/Graph/p_queue.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Богдан Воробієнко on 30.05.2022.
-//
-
-#ifndef GRAPH_P_QUEUE_H
-#define GRAPH_P_QUEUE_H
-
-#endif //GRAPH_P_QUEUE_H
