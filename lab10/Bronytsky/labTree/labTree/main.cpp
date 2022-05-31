b2570a0e4e3aa8419a9301d5a1f16eb1b9c2d0e4
Bronytsky-D <b.dima1916@gmail.com>
2022-05-31 08:41:42 +0300

    add task 10

---
+++ lab10/Bronytsky/labTree/labTree/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include"Tree.h"
+
+int main()
+{
+	btree* tree = new btree();
+
+	tree->insert(10);
+	tree->insert(6);
+	tree->insert(14);
+	tree->insert(5);
+	tree->insert(8);
+	tree->insert(11);
+	tree->insert(18);
+
+	tree->preorder_print();
+	tree->inorder_print();
+	tree->postorder_print();
+
+	delete tree;
+}
